6740ebd4de23fd5aa8c511652f451f41cc9752c7
David Rubert <david.rubert@gmail.com>
2026-05-04 18:00:59 +0200

    data: history 2026-05-03

--- generate_history.sh
+++ generate_history.sh
@@ -9,7 +9,7 @@ cd "$BASE_DIR"
 DATE_ARG="${1:-today}"
 DATE_STR=$(date -d "$DATE_ARG" "+%Y-%m-%d")
 
-echo "$BASE_DIR/.venv/bin/python" generate_history.py "$DATE_STR"
+"$BASE_DIR/.venv/bin/python" generate_history.py "$DATE_STR"
 
 git add -A
 git commit -q -m "data: history $DATE_STR"
